b42577890ce92506b1ca4182fcfe28d0e8806947
kimsinok0714 <javaenjoy0714@gmail.com>
2025-09-22 17:31:43 +0900

    Update ArticleService.java

--- boardapp/src/main/java/com/example/boardapp/service/ArticleService.java
+++ boardapp/src/main/java/com/example/boardapp/service/ArticleService.java
@@ -1,23 +1,21 @@
 package com.example.boardapp.service;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.example.boardapp.domain.ArticleDto;
 import com.example.boardapp.mapper.ArticleMapper;
 import com.example.boardapp.util.Criteria;
-
 import lombok.RequiredArgsConstructor;
 
+// 트랜잭션을 설정하지 않는 경우는 무조건 커밋됩니다.
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class ArticleService {
-
+    // field
     private final ArticleMapper articleMapper;
 
-
+    // 비즈니스 메소드
     public List<ArticleDto> retrieveArticleList() {
         return articleMapper.selectArticleList();
     }
