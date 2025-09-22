5ec9e9884180912a1116d8dd3d09a15d91cfd02c
kimsinok0714 <javaenjoy0714@gmail.com>
2025-09-22 17:34:15 +0900

    Update ArticleController.java

--- boardapp/src/main/java/com/example/boardapp/controller/ArticleController.java
+++ boardapp/src/main/java/com/example/boardapp/controller/ArticleController.java
@@ -2,19 +2,15 @@ package com.example.boardapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.boardapp.domain.ArticleDto;
 import com.example.boardapp.service.ArticleService;
 import com.example.boardapp.util.Criteria;
-
 import io.micrometer.core.ipc.http.HttpSender.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,23 +20,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/v1")
 public class ArticleController {
-
+    // field
     private final ArticleService articleService;
-
 
     @GetMapping("/articles")
     public ResponseEntity<ArticleDto> getArticleList() {
-
-        return new ResponseEntity(articleService.retrieveArticleList(), HttpStatus.OK);
-   
+        return new ResponseEntity(articleService.retrieveArticleList(), HttpStatus.OK);   
     }
     
     @PostMapping("/articles")
